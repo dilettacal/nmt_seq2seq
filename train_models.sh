7577b9414c0d827cab291d8834f3b56998728905
dcal <s0559842@htw-berlin.de>
2019-06-26 16:28:42 +0200

    changes

--- train_models.sh
+++ train_models.sh
@@ -9,24 +9,11 @@ echo "Sutskever"
 echo "Vocabulary: 30000"
 
 echo "DE_EN"
-python3 run_custom_nmt.py --hs 1000 --emb 500 --lr 0.05 --nlayers 4 --dp 0.25 --reverse_input True --model_type s --reverse True --train 500000 --val 50000 --test 5000 --epochs 70
+python3 run_custom_nmt.py --hs 1000 --emb 500 --lr 0.05 --nlayers 4 --dp 0.25 --reverse_input True --model_type s --reverse True --train 300000 --val 30000 --test 3000 --epochs 70 -v 30000
 
 echo "EN_DE"
-python3 run_custom_nmt.py --hs 1000 --emb 500 --lr 0.05 --nlayers 4 --dp 0.25 --reverse_input True --model_type s --reverse False --train 500000 --val 50000 --test 5000 --epochs 70
+python3 run_custom_nmt.py --hs 1000 --emb 500 --lr 0.05 --nlayers 4 --dp 0.25 --reverse_input True --model_type s --reverse False --train 300000 --val 30000 --test 3000 --epochs 70 -v 30000
 
 echo "====================================="
-echo "Vocabulary: 50000"
 
-echo "===================================="
 
-echo "CHO"
-
-echo "Vocabulary: 30000"
-
-echo "DE_EN"
-python3 run_custom_nmt.py --hs 1000 --emb 500 --lr 0.05 --nlayers 1 --dp 0.25 --reverse_input False --model_type c --reverse True --train 500000 --val 50000 --test 5000 --epochs 70
-
-echo "EN_DE"
-python3 run_custom_nmt.py --hs 1000 --emb 500 --lr 0.05 --nlayers 1 --dp 0.25 --reverse_input False --model_type c --reverse False --train 500000 --val 50000 --test 5000 --epochs 70
-
-echo "====================================="
