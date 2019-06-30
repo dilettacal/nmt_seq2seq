296a399dc06672fb08bd881930a0e3164f4da02a
dcal <s0559842@htw-berlin.de>
2019-06-30 09:02:02 +0200

    test 2500

--- cho_train_models.sh
+++ cho_train_models.sh
@@ -6,7 +6,7 @@ echo "EN_DE"
 echo "CHO"
 echo "====================================="
 
-python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.5 --reverse_input False --model_type c --reverse False  --epochs 70 -v 30000 -b 64 --train 250000 --val 25000 --test 25000
+python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.5 --reverse_input False --model_type c --reverse False  --epochs 70 -v 30000 -b 64 --train 250000 --val 25000 --test 2500
 echo "====================================="
 
 python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.0 --reverse_input False --model_type c --reverse False --epochs 70 -v 30000 -b 64 --train 250000 --val 25000 --test 2500
