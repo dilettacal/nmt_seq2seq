218c3049b3536d66c42f8b72e2a10affa83f0f4a
dcal <s0559842@htw-berlin.de>
2019-06-30 10:29:05 +0200

    batch size

--- cho_train_models.sh
+++ cho_train_models.sh
@@ -6,13 +6,13 @@ echo "EN_DE"
 echo "CHO"
 echo "====================================="
 
-python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.5 --reverse_input False --model_type c --reverse False  --epochs 70 -v 30000 -b 64 --train 250000 --val 25000 --test 2500
+python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.5 --reverse_input False --model_type c --reverse False  --epochs 70 -v 30000 -b 100 --train 250000 --val 25000 --test 2500
 echo "====================================="
 
-python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.0 --reverse_input False --model_type c --reverse False --epochs 70 -v 30000 -b 64 --train 250000 --val 25000 --test 2500
+python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.0 --reverse_input False --model_type c --reverse False --epochs 70 -v 30000 -b 100 --train 250000 --val 25000 --test 2500
 echo "====================================="
 
-python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.25 --reverse_input False --model_type c --reverse False --epochs 70 -v 30000 -b 64 --train 250000 --val 25000 --test 2500
+python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.25 --reverse_input False --model_type c --reverse False --epochs 70 -v 30000 -b 100 --train 250000 --val 25000 --test 2500
 
 echo "====================================="
 echo "====================================="
@@ -24,12 +24,12 @@ echo "DE_EN"
 echo "CHO"
 echo "====================================="
 
-python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.5 --reverse_input False --reverse True --model_type c --epochs 70 -v 30000 -b 64 --train 250000 --val 25000 --test 2500
+python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.5 --reverse_input False --reverse True --model_type c --epochs 70 -v 30000 -b 100 --train 250000 --val 25000 --test 2500
 echo "====================================="
 
-python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.0 --reverse_input False --reverse True --model_type c --epochs 70 -v 30000 -b 64 --train 250000 --val 25000 --test 2500
+python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.0 --reverse_input False --reverse True --model_type c --epochs 70 -v 30000 -b 100 --train 250000 --val 25000 --test 2500
 echo "====================================="
 
-python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.25 --reverse_input False --reverse True --model_type c --epochs 70 -v 30000 -b 64 --train 250000 --val 25000 --test 2500
+python3 run_custom_nmt.py --hs 1000 --emb 500 --nlayers 1 --dp 0.25 --reverse_input False --reverse True --model_type c --epochs 70 -v 30000 -b 100 --train 250000 --val 25000 --test 2500
 echo "====================================="
 echo "====================================="
--- en_train_models.sh
+++ en_train_models.sh
@@ -8,15 +8,15 @@ echo "Sutskever"
 
 echo "====================================="
 
-python3 run_custom_nmt.py --hs 500 --emb 500 --nlayers 4 --dp 0.5 --reverse_input True --model_type s --reverse False --epochs 70 -v 30000 --train 250000 --val 25000 --test 2500 -b 64
+python3 run_custom_nmt.py --hs 500 --emb 500 --nlayers 4 --dp 0.5 --reverse_input True --model_type s --reverse False --epochs 70 -v 30000 --train 250000 --val 25000 --test 2500 -b 100
 
 echo "====================================="
 
-python3 run_custom_nmt.py --hs 500 --emb 500 --nlayers 4 --dp 0.25 --reverse_input True --model_type s --reverse False  --epochs 70 -v 30000 --train 250000 --val 25000 --test 2500 -b 64
+python3 run_custom_nmt.py --hs 500 --emb 500 --nlayers 4 --dp 0.25 --reverse_input True --model_type s --reverse False  --epochs 70 -v 30000 --train 250000 --val 25000 --test 2500 -b 100
 
 echo "====================================="
 
-python3 run_custom_nmt.py --hs 500 --emb 500 --nlayers 4 --dp 0.0 --reverse_input True --model_type s --reverse False --epochs 70 -v 30000 --train 250000 --val 25000 --test 2500 -b 64
+python3 run_custom_nmt.py --hs 500 --emb 500 --nlayers 4 --dp 0.0 --reverse_input True --model_type s --reverse False --epochs 70 -v 30000 --train 250000 --val 25000 --test 2500 -b 100
 
 
 echo "====================================="
@@ -30,15 +30,15 @@ echo "Bidirectional"
 
 echo "====================================="
 
-python3 run_custom_nmt.py --hs 500 --emb 500 --nlayers 4 --dp 0.5 --bi True --model_type custom --reverse False --epochs 70 -v 30000 --train 250000 --val 25000 --test 2500 -b 64
+python3 run_custom_nmt.py --hs 500 --emb 500 --nlayers 4 --dp 0.5 --bi True --model_type custom --reverse False --epochs 70 -v 30000 --train 250000 --val 25000 --test 2500 -b 100
 
 echo "====================================="
 
-python3 run_custom_nmt.py --hs 500 --emb 500 --nlayers 4 --dp 0.0 --bi True --model_type custom --reverse False --epochs 70 -v 30000 --train 250000 --val 25000 --test 2500 -b 64
+python3 run_custom_nmt.py --hs 500 --emb 500 --nlayers 4 --dp 0.0 --bi True --model_type custom --reverse False --epochs 70 -v 30000 --train 250000 --val 25000 --test 2500 -b 100
 
 echo "====================================="
 
-python3 run_custom_nmt.py --hs 500 --emb 500 --nlayers 4 --dp 0.25 --bi True --model_type custom --reverse False --epochs 70 -v 30000 --train 250000 --val 25000 --test 2500 -b 64
+python3 run_custom_nmt.py --hs 500 --emb 500 --nlayers 4 --dp 0.25 --bi True --model_type custom --reverse False --epochs 70 -v 30000 --train 250000 --val 25000 --test 2500 -b 100
 echo "====================================="
 
 echo "====================================="
