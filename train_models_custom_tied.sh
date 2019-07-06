c4bcbea00bea8f0eaffbb6784b0cdf52231383d2
dcal <s0559842@htw-berlin.de>
2019-07-06 18:44:53 +0200

    tied script

---
+++ train_models_custom_tied.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+
+echo "Starting script.........."
+
+echo "DE-EN"
+
+echo "Sutskever"
+
+echo "====================================="
+
+python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1000 --test 1000 --lr 0.002 --tied True
+
+echo "====================================="
+
+echo "Improvements"
+
+python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64  --train 170000 --val 1000 --test 1000 --lr 0.002 --tied True
+
+echo "====================================="
+
+echo "====================================="
+
+
+echo "CUSTOM"
+
+echo "Bidirectional"
+
+echo "====================================="
+
+python3 run_custom_nmt.py --hs 300 --emb 300  --nlayers 2 --dp 0.25 --bi True --model_type custom --reverse False --epochs 80 -v 30000 -b 64 --train 170000 --val 1000 --test 1000 --lr 0.002 --tied True
+echo "====================================="
+
+
+echo "Improvements"
+
+echo "====================================="
+
+python3 run_custom_nmt.py --hs 300 --emb 300  --nlayers 4 --dp 0.25 --bi True --model_type custom --reverse False --epochs 80 -v 30000 -b 64  --train 170000 --val 1000 --test 1000 --lr 0.002 --tied True
+echo "====================================="
+echo "====================================="
+
+
+echo "Attention experiments"
+
+echo "Additive"
+
+### alterna en/de
+
+echo "====================================="
+
+python3 run_custom_nmt.py --hs 300 --emb 300  --nlayers 2 --dp 0.25 --bi False --model_type custom --reverse True --epochs 80 -v 30000 -b 64 --train 170000 --val 1000 --test 1000 --attn additive --lr 0.002 --tied True
+
+python3 run_custom_nmt.py --hs 300 --emb 300  --nlayers 2 --dp 0.25 --bi True --model_type custom --reverse True --epochs 80 -v 30000 -b 64 --train 170000 --val 1000 --test 1000 --attn additive --lr 0.002 --tied True
+
+
+##### 4 layers
+
+python3 run_custom_nmt.py --hs 300 --emb 300  --nlayers 4 --dp 0.25 --bi False --model_type custom --reverse True --epochs 80 -v 30000 -b 64 --train 170000 --val 1000 --test 1000 --attn additive --lr 0.002 --tied True
+
+python3 run_custom_nmt.py --hs 300 --emb 300  --nlayers 4 --dp 0.25 --bi True --model_type custom --reverse True --epochs 80 -v 30000 -b 64 --train 170000 --val 1000 --test 1000 --attn additive --lr 0.002 --tied True
+
+
+echo "DOT"
+
+python3 run_custom_nmt.py --hs 300 --emb 300  --nlayers 2 --dp 0.25 --bi False --model_type custom --reverse True --epochs 80 -v 30000 -b 64 --train 170000 --val 1000 --test 1000 --attn dot --lr 0.002 --tied True
+
+python3 run_custom_nmt.py --hs 300 --emb 300  --nlayers 2 --dp 0.25 --bi True --model_type custom --reverse True --epochs 80 -v 30000 -b 64 --train 170000 --val 1000 --test 1000 --attn dot --lr 0.002 --tied True
+
+
+##### 4 layers
+
+python3 run_custom_nmt.py --hs 300 --emb 300  --nlayers 4 --dp 0.25 --bi False --model_type custom --reverse True --epochs 80 -v 30000 -b 64 --train 170000 --val 1000 --test 1000 --attn dot --lr 0.002 --tied True
+
+python3 run_custom_nmt.py --hs 300 --emb 300  --nlayers 4 --dp 0.25 --bi True --model_type custom --reverse True --epochs 80 -v 30000 -b 64 --train 170000 --val 1000 --test 1000 --attn dot --lr 0.002 --tied True
+
+
