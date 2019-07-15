b1cea62106baae8e6f392e82d90e0514cd26de76
dcal <s0559842@htw-berlin.de>
2019-07-15 13:27:12 +0200

    Selected best baseline models based on beam results and bleu

--- scripts/baseline_search/baseline_dropout_gru.sh
+++ scripts/baseline_search/baseline_dropout_gru.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd ..
+
 echo "Baseline model based on Sutskever"
 echo "Fixed params: 300 embedding, hidden, patience:10"
 echo "Language combination: German>English, dataset: Europarl"
@@ -7,6 +9,8 @@ echo "No pretraiend, no tied"
 echo "Test: Dropout values on 2 and 4 layers"
 
 echo "LR 2"
+
+##########################  Almost bad on every experiment ########################################################################################################################################################################
 
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.002 --tok tok --tied False --rnn gru
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.50 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.002 --tok tok --tied False --rnn gru
@@ -19,17 +23,24 @@ python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.0 --reverse_inpu
 
 echo "Reducing learning rate to 0.0002"
 
+
+##########################  Best 0.0002, dp 0.25, layer 2, BLEU: 14,5 for Beam 5 ##################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --tied False --rnn gru
+###################################################################################################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.50 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --tied False --rnn gru
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.0 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --tied False --rnn gru
 
+##########################  Best 0.0002, dp 0.25, layer 4,  BLEU: 15,88 for Beam 5 ################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --tied False --rnn gru
+###################################################################################################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.50 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --tied False --rnn gru
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.0 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --tied False --rnn gru
 
 
 
 echo "LR 3"
+
+##########################  Almost bad on every experiment ########################################################################################################################################################################
 
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.003 --tok tok --tied False --rnn gru
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.50 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.003 --tok tok --tied False --rnn gru
@@ -42,10 +53,15 @@ python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.0 --reverse_inpu
 
 echo "Reducing learning rate to 0.0003"
 
+##########################  Best 0.0003, dp 0.25, layer 2, BLEU: 14,91 for Beam 5 ##################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --tied False --rnn gru
+####################################################################################################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.50 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --tied False --rnn gru
-python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.0 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --tied False
+python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.0 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --tied False --rnn gru
 
+
+#########################  Best 0.0003, dp 0.25, layer 4, BLEU: 15,5 for Beam 5 ##################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --tied False --rnn gru
+##################################################################################################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.50 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --tied False --rnn gru
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.0 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --tied False --rnn gru
--- scripts/baseline_search/baseline_dropout_lstm.sh
+++ scripts/baseline_search/baseline_dropout_lstm.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd ..
+
 echo "Baseline model based on Sutskever"
 echo "Fixed params: 300 embedding, hidden, patience:10"
 echo "Language combination: German>English, dataset: Europarl"
@@ -8,28 +10,38 @@ echo "Test: Dropout values on 2 and 4 layers"
 
 echo "LR 2"
 
+##########################  Best 0.002, dp 0.25, layer 2, BLEU: 14,5 for Beam 5 ########################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.002 --tok tok --tied False
+########################################################################################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.50 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.002 --tok tok --tied False
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.0 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.002 --tok tok --tied False
 
+##########################  Best 0.002, dp 0.25, layer 4, BLEU: 14,33 for Beam 5 ########################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.002 --tok tok --tied False
+########################################################################################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.50 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.002 --tok tok --tied False
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.0 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.002 --tok tok --tied False
 
 
 echo "Reducing learning rate to 0.0002"
 
+##########################  Best 0.0002, dp 0.25, layer 2, BLEU: 15,5 for Beam 5 ########################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --tied False
+########################################################################################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.50 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --tied False
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.0 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --tied False
 
+##########################  Best 0.0002, dp 0.25, layer 4, BLEU: 17,11 for Beam 5 ########################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --tied False
+########################################################################################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.50 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --tied False
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.0 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --tied False
 
 
 
 echo "LR 3"
+
+####################################################################### Almost bad on every experiment #################################################################################################################
 
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.003 --tok tok --tied False
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.50 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.003 --tok tok --tied False
@@ -42,10 +54,14 @@ python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.0 --reverse_inpu
 
 echo "Reducing learning rate to 0.0003"
 
+##########################  Best 0.0003, dp 0.25, layer 2, BLEU: 15,73 for Beam 5 ########################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --tied False
+########################################################################################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.50 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --tied False
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.0 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --tied False
 
+##########################  Best 0.0003, dp 0.25, layer 4, BLEU: 17,93 for Beam 5 ########################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --tied False
+########################################################################################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.50 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --tied False
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.0 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --tied False
