4e945dbc23734c30c693852ca949edf2c110e6f0
dcal <s0559842@htw-berlin.de>
2019-07-18 15:37:04 +0200

    Kleinigkeiten

--- preprocess.py
+++ preprocess.py
@@ -39,7 +39,7 @@ from settings import DATA_DIR_PREPRO
 def data_prepro_parser():
     parser = argparse.ArgumentParser(description='Neural Machine Translation')
     parser.add_argument("--lang_code", default="de", type=str)
-    parser.add_argument("--type", default="tmx", type=str, help="TMX")
+    #  parser.add_argument("--type", default="tmx", type=str, help="TMX")
     parser.add_argument("--corpus", default="europarl", type=str, help="Corpus name")
     parser.add_argument("--max_len", default=30, type=int, help="Filter sequences with a length <= max_len")
     parser.add_argument("--min_len", default=2, type=int, help="Filter sequences with a length >= min_len")
@@ -54,7 +54,7 @@ def raw_preprocess(parser):
     maybe_download_and_extract_europarl(language_code=parser.lang_code, tmx=True)
     corpus_name = parser.corpus
     lang_code = parser.lang_code
-    file_type = parser.type
+    # file_type = parser.type
     path_to_raw_file = parser.path
     max_len, min_len = parser.max_len, parser.min_len
 
@@ -63,97 +63,96 @@ def raw_preprocess(parser):
     STORE_PATH = os.path.join(os.path.expanduser(DATA_DIR_PREPRO), corpus_name, lang_code, "splits", str(max_len))
     os.makedirs(STORE_PATH, exist_ok=True)
 
-    assert file_type in ["tmx", "txt"]
+    start = time.time()
+    output_file_path = os.path.join(DATA_DIR_PREPRO, corpus_name, lang_code)
 
-    if file_type == "tmx":
-        start = time.time()
-        output_file_path = os.path.join(DATA_DIR_PREPRO, corpus_name, lang_code)
-
-        files = [file for file in os.listdir(output_file_path) if file.startswith("bitext")]
-        if "bitext.en" in files and "bitext.{}".format(lang_code) in files:
-            print("TMX already converted!")
-        else:
-            print("Converting tmx to file...")
-            ### convert tmx to plain texts - no tokenization is performed
-            converter = Converter(output=FileOutput(output_file_path))
-            converter.convert([COMPLETE_PATH])
-            print("Converted lines:", converter.output_lines)
-
-        target_file = "bitext.{}".format(lang_code)
-        src_lines, trg_lines = [], []
-
-        with open(os.path.join(output_file_path, "bitext.en"), 'r') as src_file, \
-                open(os.path.join(output_file_path, target_file), 'r') as target_file:
-            for src_line, trg_line in zip(src_file, target_file):
-                src_line = src_line.strip()
-                trg_line = trg_line.strip()
-                if src_line != "" and trg_line != "":
-                    src_lines.append(src_line)
-                    trg_lines.append(trg_line)
-
-        ### tokenize lines ####
-        assert len(src_lines) == len(trg_lines), "Different lengths!"
-
-        src_tokenizer, trg_tokenizer = get_custom_tokenizer("en", "w", spacy_pretok=False), get_custom_tokenizer("de",
-                                                                                                                 "w",
-                                                                                                                 spacy_pretok=False)  # spacy is used
-        src_logger = Logger(output_file_path, file_name="bitext.tok.en")
-        trg_logger = Logger(output_file_path, file_name="bitext.tok.{}".format(lang_code))
-
-        temp_src_toks, temp_trg_toks = [], []
-
-        with src_tokenizer.nlp.disable_pipes('ner'):
-            for i, doc in enumerate(src_tokenizer.nlp.pipe(src_lines, batch_size=1000)):
-                tok_doc = ' '.join([tok.text for tok in doc])
-                temp_src_toks.append(tok_doc)
-                src_logger.log(tok_doc, stdout=True if i % 100000 == 0 else False)
-
-        with trg_tokenizer.nlp.disable_pipes('ner'):
-            for i, doc in enumerate(trg_tokenizer.nlp.pipe(trg_lines, batch_size=1000)):
-                tok_doc = ' '.join([tok.text for tok in doc])
-                temp_trg_toks.append(tok_doc)
-                trg_logger.log(tok_doc, stdout=True if i % 100000 == 0 else False)
-
-        if max_len > 0:
-            files = ['.'.join(file.split(".")[:2]) for file in os.listdir(STORE_PATH) if
-                     file.endswith("tok.en") or file.endswith("tok." + lang_code)]
-            filtered_src_lines, filtered_trg_lines = [], []
-            if files:
-                print("File already reduced by length!")
-            else:
-                print("Filtering by length...")
-                filtered_src_lines, filtered_trg_lines = [], []
-                for src_l, trg_l in zip(temp_src_toks, temp_trg_toks):
-                    # src_l_s = src_l.strip()
-                    # trg_l_s = trg_l.strip()
-                    ### remove possible duplicate spaces
-                    src_l_s = re.sub(' +', ' ', src_l)
-                    trg_l_s = re.sub(' +', ' ', trg_l)
-                    if src_l_s != "" and trg_l_s != "":
-                        src_l_spl, trg_l_spl = src_l_s.split(" "), trg_l_s.split(" ")
-                        if len(src_l_spl) <= max_len and len(trg_l_spl) <= max_len:
-                            if len(src_l_spl) >= min_len and len(trg_l_spl) >= min_len:
-                                filtered_src_lines.append(' '.join(src_l_spl))
-                                filtered_trg_lines.append(' '.join(trg_l_spl))
-
-            assert len(filtered_src_lines) == len(filtered_trg_lines)
-
-            src_lines, trg_lines = filtered_src_lines, filtered_trg_lines
-            print("Splitting files...")
-            train_data, val_data, test_data, samples_data = split_data(src_lines, trg_lines)
-            persist_txt(train_data, STORE_PATH, "train.tok", exts=(".en", "." + lang_code))
-            persist_txt(val_data, STORE_PATH, "val.tok", exts=(".en", "." + lang_code))
-            persist_txt(test_data, STORE_PATH, "test.tok", exts=(".en", "." + lang_code))
-            print("Generating samples files...")
-            persist_txt(samples_data, STORE_PATH, file_name="samples.tok", exts=(".en", "." + lang_code))
-
-        print("Total time:", convert(time.time() - start))
+    files = [file for file in os.listdir(output_file_path) if file.startswith("bitext")]
+    if "bitext.en" in files and "bitext.{}".format(lang_code) in files:
+        print("TMX already converted!")
     else:
-        # TODO
-        pass
+        print("Converting tmx to file...")
+        ### convert tmx to plain texts - no tokenization is performed
+        converter = Converter(output=FileOutput(output_file_path))
+        converter.convert([COMPLETE_PATH])
+        print("Converted lines:", converter.output_lines)
+
+    target_file = "bitext.{}".format(lang_code)
+    src_lines, trg_lines = [], []
+
+    with open(os.path.join(output_file_path, "bitext.en"), 'r') as src_file, \
+            open(os.path.join(output_file_path, target_file), 'r') as target_file:
+        for src_line, trg_line in zip(src_file, target_file):
+            src_line = src_line.strip()
+            trg_line = trg_line.strip()
+            if src_line != "" and trg_line != "":
+                src_lines.append(src_line)
+                trg_lines.append(trg_line)
+
+    ### tokenize lines ####
+    assert len(src_lines) == len(trg_lines), "Different lengths!"
+
+    src_tokenizer, trg_tokenizer = get_custom_tokenizer("en", "w", spacy_pretok=False), get_custom_tokenizer("de",
+                                                                                                             "w",
+                                                                                                             spacy_pretok=False)  # spacy is used
+    src_logger = Logger(output_file_path, file_name="bitext.tok.en")
+    trg_logger = Logger(output_file_path, file_name="bitext.tok.{}".format(lang_code))
+
+    temp_src_toks, temp_trg_toks = [], []
+
+    with src_tokenizer.nlp.disable_pipes('ner'):
+        for i, doc in enumerate(src_tokenizer.nlp.pipe(src_lines, batch_size=1000)):
+            tok_doc = ' '.join([tok.text for tok in doc])
+            temp_src_toks.append(tok_doc)
+            src_logger.log(tok_doc, stdout=True if i % 100000 == 0 else False)
+
+    with trg_tokenizer.nlp.disable_pipes('ner'):
+        for i, doc in enumerate(trg_tokenizer.nlp.pipe(trg_lines, batch_size=1000)):
+            tok_doc = ' '.join([tok.text for tok in doc])
+            temp_trg_toks.append(tok_doc)
+            trg_logger.log(tok_doc, stdout=True if i % 100000 == 0 else False)
+
+    if max_len > 0:
+        files = ['.'.join(file.split(".")[:2]) for file in os.listdir(STORE_PATH) if
+                 file.endswith("tok.en") or file.endswith("tok." + lang_code)]
+        filtered_src_lines, filtered_trg_lines = [], []
+        if files:
+            print("File already reduced by length!")
+        else:
+            print("Filtering by length...")
+            filtered_src_lines, filtered_trg_lines = [], []
+            for src_l, trg_l in zip(temp_src_toks, temp_trg_toks):
+                ### remove possible duplicate spaces
+                src_l_s = re.sub(' +', ' ', src_l)
+                trg_l_s = re.sub(' +', ' ', trg_l)
+                if src_l_s != "" and trg_l_s != "":
+                    src_l_spl, trg_l_spl = src_l_s.split(" "), trg_l_s.split(" ")
+                    if len(src_l_spl) <= max_len and len(trg_l_spl) <= max_len:
+                        if len(src_l_spl) >= min_len and len(trg_l_spl) >= min_len:
+                            filtered_src_lines.append(' '.join(src_l_spl))
+                            filtered_trg_lines.append(' '.join(trg_l_spl))
+
+        assert len(filtered_src_lines) == len(filtered_trg_lines)
+
+        src_lines, trg_lines = filtered_src_lines, filtered_trg_lines
+        print("Splitting files...")
+        train_data, val_data, test_data, samples_data = split_data(src_lines, trg_lines)
+        persist_txt(train_data, STORE_PATH, "train.tok", exts=(".en", "." + lang_code))
+        persist_txt(val_data, STORE_PATH, "val.tok", exts=(".en", "." + lang_code))
+        persist_txt(test_data, STORE_PATH, "test.tok", exts=(".en", "." + lang_code))
+        print("Generating samples files...")
+        persist_txt(samples_data, STORE_PATH, file_name="samples.tok", exts=(".en", "." + lang_code))
+    else:
+
+        print("Splitting files...")
+        train_data, val_data, test_data, samples_data = split_data(src_lines, trg_lines)
+        persist_txt(train_data, STORE_PATH, "train.tok", exts=(".en", "." + lang_code))
+        persist_txt(val_data, STORE_PATH, "val.tok", exts=(".en", "." + lang_code))
+        persist_txt(test_data, STORE_PATH, "test.tok", exts=(".en", "." + lang_code))
+        print("Generating samples files...")
+        persist_txt(samples_data, STORE_PATH, file_name="samples.tok", exts=(".en", "." + lang_code))
+
+    print("Total time:", convert(time.time() - start))
 
 
 if __name__ == '__main__':
     raw_preprocess(data_prepro_parser().parse_args())
-
-
