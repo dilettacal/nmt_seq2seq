0ddc6aaeeac1e4318388c8f3b04052315badb7d9
Di Cal <31538342+dltcls@users.noreply.github.com>
2019-07-19 07:09:47 +0200

    windows config

---
+++ windows_setup.bat
@@ -0,0 +1,29 @@
+# Create virtualenv
+python -m virtualenv env
+cd ./env/Scripts
+./activate
+## Install dependencies ###
+
+#### PyTorch stuff #####
+pip3 install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp37-cp37m-win_amd64.whl
+pip3 install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp37-cp37m-win_amd64.whl
+pip install torchtext
+
+#### NLP Stuff ####
+pip install -U spacy
+python -m spacy download en
+python -m spacy download de
+python -m spacy download fr
+python -m spacy download es
+python -m spacy download pt
+python -m spacy download it
+python -m spacy download xx
+
+pip install -U nltk
+python -m nltk.downloader all
+
+
+#### datascience stuff ###
+
+pip install -U numpy matplotlib pandas seaborn scipy scikit-learn dill
+
