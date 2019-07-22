f3488a83c72eb5a6edc13a661b1deda252d66fce
Diletta Calussi <s0559842@htw-berlin.de>
2019-07-22 10:15:16 +0200

    Ripristinato file

--- scripts/baseline_best_5_tied/baseline_best_5_gru_tied.sh
+++ scripts/baseline_best_5_tied/baseline_best_5_gru_tied.sh
@@ -11,7 +11,7 @@ python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --reverse_inp
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True
 
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0001 --tok tok --rnn gru --beam 5 --tied True
-python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0001--tok tok --rnn gru --beam 5 --tied True
+python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0001 --tok tok --rnn gru --beam 5 --tied True
 
 ##########################  Best 0.0003, dp 0.25, layer 2, BLEU: 14,91 for Beam 5 ##################################################################################################################################################
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --reverse_input True --reverse True --model_type s --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0003 --tok tok --rnn gru --beam 5 --tied True
