b6fbd745bed04c6e5b481ca22c4e18b4484ceb7f
Diletta Calussi <s0559842@htw-berlin.de>
2019-07-23 08:09:27 +0200

    Checking every 10

--- project/utils/training.py
+++ project/utils/training.py
@@ -35,7 +35,7 @@ def train_model(train_iter, val_iter, model, criterion, optimizer, scheduler, ep
     nltk_bleus, perl_bleus = [], []
     bleus = dict()
 
-    check_transl_every = check_translations_every
+    check_transl_every = check_translations_every if epochs <= 80 else check_translations_every*2
 
     mini_samples = [batch for i, batch in enumerate(samples_iter) if i < 3]
 
