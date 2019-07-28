db3d0c8091dca2bd27e228ef1ea23c4292f9927e
dcal <s0559842@htw-berlin.de>
2019-07-28 23:04:28 +0200

    win requirements

--- windows_setup.bat
+++ windows_setup.bat
@@ -1,29 +1,39 @@
 @echo off
 :: Create virtualenv
+:: Type these commands before running this script!
+:: Install a new virtual environment
 :: python -m virtualenv env
-:: activate environment:
+:: Then activate environment:
 :: ../env/Scripts/activate
 :: Install dependencies
 
 :: PyTorch stuff
 pip3 install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp37-cp37m-win_amd64.whl
-pip3 install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp37-cp37m-win_amd64.whl
+:: pip3 install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp37-cp37m-win_amd64.whl
 pip install torchtext
 
-:: NLP
+:: spaCy
+:: Uncomment languages if needed
 pip install -U spacy
+:: Standard training requires englis
 python -m spacy download en
+:: My model is trained german > english
 python -m spacy download de
-python -m spacy download fr
-python -m spacy download es
-python -m spacy download pt
-python -m spacy download it
+:: This is the multi-language model
 python -m spacy download xx
+:: Other models :::
+:: python -m spacy download fr
+:: python -m spacy download es
+:: python -m spacy download pt
+:: python -m spacy download it
 
+:: Needed for bleu_score
 pip install -U nltk
-python -m nltk.downloader all
+:: Uncomment if needed
+:: python -m nltk.downloader all
 
 :: Other stuff
-
-pip install -U numpy matplotlib pandas seaborn scipy scikit-learn dill
+:: dill is needed to serialize objects
+pip install -U dill
+pip install -U numpy matplotlib pandas seaborn scipy scikit-learn
 
