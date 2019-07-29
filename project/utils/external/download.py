0680a9cd7febf19fb55c764bf85363cb68344739
dcal <s0559842@htw-berlin.de>
2019-07-29 17:51:57 +0200

    Code comments external

--- project/utils/external/download.py
+++ project/utils/external/download.py
@@ -1,17 +1,7 @@
 ########################################################################
-#
 # Functions for downloading and extracting data-files from the internet.
-#
-# Implemented in Python 3.5
-#
-########################################################################
-#
 # This file is part of the TensorFlow Tutorials available at:
-#
 # https://github.com/Hvass-Labs/TensorFlow-Tutorials
-#
-# Published under the MIT License. See the file LICENSE for details.
-#
 # Copyright 2016 by Magnus Erik Hvass Pedersen
 #
 ########################################################################
@@ -23,23 +13,18 @@ import urllib.request
 import tarfile
 import zipfile
 
-########################################################################
 
 def _print_download_progress(count, block_size, total_size):
     """
     Function used for printing the download progress.
     Used as a call-back function in maybe_download_and_extract().
     """
-
     # Percentage completion.
     pct_complete = float(count * block_size) / total_size
-
     # Limit it because rounding errors may cause it to exceed 100%.
     pct_complete = min(1.0, pct_complete)
-
     # Status-message. Note the \r which means the line should overwrite itself.
     msg = "\r- Download progress: {0:.1%}".format(pct_complete)
-
     # Print it.
     sys.stdout.write(msg)
     sys.stdout.flush()
@@ -56,16 +41,13 @@ def download(base_url, filename, download_dir):
     :param download_dir: Local directory for storing the file.
     :return: Nothing.
     """
-
     # Path for local file.
     save_path = os.path.join(download_dir, filename)
-
     # Check if the file already exists, otherwise we need to download it now.
     if not os.path.exists(save_path):
         # Check if the download directory exists, otherwise create it.
         if not os.path.exists(download_dir):
             os.makedirs(download_dir)
-
         print("Downloading", filename, "...")
 
         # Download the file from the internet.
@@ -79,24 +61,22 @@ def download(base_url, filename, download_dir):
 
 def maybe_download_and_extract(url, download_dir, language_code="de", raw_file="{}-en.tmx"):
     """
+    This function has been modified to handle tmx files.
+
     Download and extract the data if it doesn't already exist.
     Assumes the url is a tar-ball file.
-
     :param url:
         Internet URL for the tar-file to download.
         Example: "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
 
     :param download_dir:
         Directory where the downloaded file is saved.
-        Example: "data/CIFAR-10/"
-
     :return:
         Nothing.
     """
 
     filename = url.split('/')[-1]
     file_path = os.path.join(download_dir, filename) #e.g. ./data/raw/europarl/de/en-de.tmx.gz
- #   print("File path:", file_path)
     if not os.path.exists(file_path):
         # Check if the download directory exists, otherwise create it.
         if not os.path.exists(download_dir):
@@ -114,13 +94,11 @@ def maybe_download_and_extract(url, download_dir, language_code="de", raw_file="
 
 
         if file_path.endswith(".zip"):
-            # Unpack the zip-file.
             zipfile.ZipFile(file=file_path, mode="r").extractall(download_dir)
         elif file_path.endswith((".tar.gz", ".tgz")):
-            # Unpack the tar-ball.
             tarfile.open(name=file_path, mode="r:gz").extractall(download_dir)
         elif file_path.endswith(".gz"):
-          ###### Modified for tmx files ############
+          # Modified for tmx files #
             raw_file = raw_file.format(language_code) # e.g. de-en.tmx
             download_dir = os.path.join(download_dir, raw_file)
             with gzip.open(file_path, 'rb') as gz:
