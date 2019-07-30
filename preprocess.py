fee2c186c10c8551ec012a295975fc6940b591cb
dcal <s0559842@htw-berlin.de>
2019-07-30 09:30:35 +0200

    Refactoring

--- preprocess.py
+++ preprocess.py
@@ -22,7 +22,7 @@ from settings import DATA_DIR_PREPRO, DATA_DIR_RAW
 
 
 def data_prepro_parser():
-    parser = argparse.ArgumentParser(description='Preprocess Europarl Dataset for NMT')
+    parser = argparse.ArgumentParser(description='Preprocess Europarl Dataset for NMT. This script allows you to preprocess and tokenize the Europarl Dataset')
     parser.add_argument("--lang_code", default="de", type=str, help="First language is English. Specifiy with 'lang_code' the second language as language code (e.g. 'de').")
     return parser
 
