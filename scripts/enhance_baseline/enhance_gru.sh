46ff7845d1b1f6ea047d3437026b30f10f346ca2
dcal <s0559842@htw-berlin.de>
2019-08-01 08:16:11 +0200

    correct script

--- scripts/enhance_baseline/enhance_gru.sh
+++ scripts/enhance_baseline/enhance_gru.sh
@@ -18,5 +18,5 @@ echo "Experiments for GRU done locally!"
 #python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.25 --bi True --reverse True --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True --pretrained True --attn additive
 ### local ###
 
-echo "Saturate optional model"
+#echo "Saturate optional model"
 #python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 4 --dp 0.25 --bi True --reverse True --epochs 240 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True --pretrained True --attn additive
