871bb71a1dbab15655afdb2701680ecbaf0f59a0
dcal <s0559842@htw-berlin.de>
2019-08-02 07:18:13 +0200

    tests

--- scripts/enhance_baseline/bidirectional.sh
+++ scripts/enhance_baseline/bidirectional.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "Bidirectional 2 layers"
+
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --bi True --reverse True --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True
 python3 run_custom_nmt.py --hs 300 --emb 300 --nlayers 2 --dp 0.25 --bi True --reverse True --epochs 80 -v 30000 -b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --rnn lstm --beam 5 --tied True
 
