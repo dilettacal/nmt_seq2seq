8f15be4a45c32fa084e8ec9fd653deef023a4f1a
dcal <s0559842@htw-berlin.de>
2019-08-03 22:11:03 +0200

    finls

--- scripts/final_models_local.sh
+++ scripts/final_models_local.sh
@@ -9,3 +9,7 @@ python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True 
 echo "Vanilla"
 python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 80 --v 30000 --b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True
 python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 80 --v 30000 --b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --rnn lstm --beam 5 --tied True
+
+echo "Pre-Vanilla"
+python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 4 --dp 0.25 --reverse True --reverse_input True --epochs 80 --v 30000 --b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True
+python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 4 --dp 0.25 --reverse_input True --reverse True --epochs 80 --v 30000 --b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --rnn lstm --beam 5 --tied True
