1a1368fb4a94fbfffeb22512d12e39d355195317
dcal <s0559842@htw-berlin.de>
2019-08-05 08:15:40 +0200

    more data

--- scripts/more_data.sh
+++ scripts/more_data.sh
@@ -4,5 +4,10 @@ echo "more data"
 python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 1 --v 30000 --b 128 --train 170000 --val 5000 --test 5000 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True --attn dot
 python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 1 --v 30000 --b 128 --train 170000 --val 5000 --test 5000 --lr 0.0002 --tok tok --rnn lstm --beam 5 --tied True --attn dot
 
+# 5 %
+python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 1 --v 30000 --b 128 --train 170000 --val 8500 --test 8500 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True --attn dot
+python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 1 --v 30000 --b 128 --train 170000 --val 8500 --test 8500 --lr 0.0002 --tok tok --rnn lstm --beam 5 --tied True --attn dot
+
+# 10 %
 python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 1 --v 30000 --b 128 --train 170000 --val 17000 --test 17000 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True --attn dot
 python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 1 --v 30000 --b 128 --train 170000 --val 17000 --test 17000 --lr 0.0002 --tok tok --rnn lstm --beam 5 --tied True --attn dot
