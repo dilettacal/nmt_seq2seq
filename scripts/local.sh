c7550c3ccc60ec29266dd14a0d4e7ff93b72ff9d
dcal <s0559842@htw-berlin.de>
2019-08-07 14:24:59 +0200

    new local files

---
+++ scripts/local.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "EN > DE"
+#python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse False --epochs 80 --v 30000 --b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True --attn dot
+python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse False --epochs 80 --v 30000 --b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --rnn lstm --beam 5 --tied True --attn dot
+
+
+echo "END ##############################"
+
+echo "OTHER DATASET"
+echo "test the model on other dataset attention"
+python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 80 --v 30000 --b 64 --lr 0.0002 --tok tok --rnn lstm --beam 10 --tied True --attn dot --corpus ""
+
+echo "Optional: test the model on other dataset"
+python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 80 --v 30000 --b 64 --lr 0.0002 --tok tok --rnn lstm --beam 10 --tied True --attn none --corpus ""
--- scripts/server.sh
+++ scripts/server.sh
@@ -5,10 +5,6 @@ echo "All training data (standard splitting)"
 python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 80 --v 30000 --b 64 --train 0 --val 5546 --test 6471 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True --attn dot
 python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 80 --v 30000 --b 64 --train 0 --val 5546 --test 6471 --lr 0.0002 --tok tok --rnn lstm --beam 5 --tied True --attn dot
 
-echo "3000 - 0,32%"
-python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 80 --v 30000 --b 64 --train 0 --val 3000 --test 3000 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True --attn dot
-python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 80 --v 30000 --b 64 --train 0 --val 3000 --test 3000 --lr 0.0002 --tok tok --rnn lstm --beam 5 --tied True --attn dot
-
 echo "END for DE > EN"
 echo "####################################"
 
