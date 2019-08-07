4df84d5194b70ced9e32463db8b2521a9cd0ce7d
dcal <s0559842@htw-berlin.de>
2019-08-07 14:25:18 +0200

    new local files

--- scripts/local.sh
+++ scripts/local.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "EN > DE"
+echo "EN > DE (lstm) - Kleine Stichprobe "
 #python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse False --epochs 80 --v 30000 --b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True --attn dot
 python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse False --epochs 80 --v 30000 --b 64 --train 170000 --val 1020 --test 1190 --lr 0.0002 --tok tok --rnn lstm --beam 5 --tied True --attn dot
 
