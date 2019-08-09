507ea16076b81b7130ba08fec2e1ae1ff7020e22
dcal <s0559842@htw-berlin.de>
2019-08-09 08:01:22 +0200

    setup bash

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-### create environment
-## Uncomment this if you want to automatically setup a virutal environment
-#python3 -m venv env
-#echo "Activating environment"
-#source "env/bin/activate"
+# Uncomment following lines to automatically setup virtual environment
+# python3 -m venv env
+# source "env/bin/activate"
 
-### Requirements for the project ####
+# Must-Dependencies
 pip install torch torchtext
 pip install -U spacy
 python3 -m spacy download en
