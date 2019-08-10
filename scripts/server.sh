03a24c1a10461b6d561181b22cb020b44b96b071
dcal <s0559842@htw-berlin.de>
2019-08-10 14:45:32 +0200

    server

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo "All training data (standard splitting)"
+#echo "All training data (standard splitting)"
 
 #python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 80 --v 30000 --b 64 --train 0 --val 5546 --test 6471 --lr 0.0002 --tok tok --rnn gru --beam 5 --tied True --attn dot
 #python3 run_custom_nmt.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 80 --v 30000 --b 64 --train 0 --val 5546 --test 6471 --lr 0.0002 --tok tok --rnn lstm --beam 5 --tied True --attn dot
 
-echo "END for DE > EN"
+#echo "END for DE > EN"
 echo "####################################"
 
 echo "EN > DE"
