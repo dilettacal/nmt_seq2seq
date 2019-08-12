3661034443e3355bd3c1ff44d3773050531ea5b0
dcal <s0559842@htw-berlin.de>
2019-08-12 10:22:09 +0200

    optionals

--- scripts/optional_tests.sh
+++ scripts/optional_tests.sh
@@ -2,9 +2,6 @@
 
 echo "Optional tests"
 
-echo "Best config + 340000 data"
-python3 train_model.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 80 --v 30000 --b 64 --train 340000 --val 2040 --test 2380 --lr 0.0002 --tok tok --rnn lstm --beam 5 --tied True --attn dot
-
 echo "Best config + full train data"
 python3 train_model.py --hs 300 --emb 300 --num_layers 2 --dp 0.25 --bi True --reverse True --epochs 80 --v 30000 --b 64 --train 0 --val 5546 --test 6471 --lr 0.0002 --tok tok --rnn lstm --beam 5 --tied True --attn dot
 
